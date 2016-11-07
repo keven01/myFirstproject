47375b4778a51f200e0bf86cf8cbfff188655173
keven01 <yangkai2018@163.com>
2016-11-07 15:09:38 +0800

    one

---
+++ src/main/java/com/example/myFirstproject/App.java
@@ -0,0 +1,17 @@
+package com.example.myFirstproject;
+
+/**
+ * Hello world!
+ *
+ */
+import org.springframework.boot.SpringApplication;  
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+  
+@SpringBootApplication  
+public class App {  
+  
+    public static void main(String[] args) {  
+    	System.out.println( "Hello World!" );
+        SpringApplication.run(App.class, args);  
+    }  
+}
