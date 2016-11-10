8b7efba37a0a8da7d1b6f2a6676fd2a7e697c011
keven01 <yangkai2018@163.com>
2016-11-10 10:21:45 +0800

    Update App.java

--- src/main/java/com/example/myFirstproject/App.java
+++ src/main/java/com/example/myFirstproject/App.java
@@ -6,12 +6,18 @@ package com.example.myFirstproject;
  */
 import org.springframework.boot.SpringApplication;  
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.boot.context.web.SpringBootServletInitializer;
   
 @SpringBootApplication  
-public class App {  
+public class App extends SpringBootServletInitializer {  
   
     public static void main(String[] args) {  
     	System.out.println( "Hello World!" );
         SpringApplication.run(App.class, args);  
     }  
+   @Override
+    protected App configure(App builder) {
+        return builder.sources(Application.class);
+    }
 }
